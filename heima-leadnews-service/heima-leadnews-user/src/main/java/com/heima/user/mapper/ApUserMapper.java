36235fc29a21652e3180cdbe1700548258d8e91d
BIBIQQ <1946865819@qq.com>
2022-01-24 14:54:22 +0800

    用户登录功能完善 访问过滤功能 请求头token布置

--- heima-leadnews-service/heima-leadnews-user/src/main/java/com/heima/user/mapper/ApUserMapper.java
+++ heima-leadnews-service/heima-leadnews-user/src/main/java/com/heima/user/mapper/ApUserMapper.java
@@ -1,6 +1,7 @@
 package com.heima.user.mapper;
 
 import com.heima.model.user.pojos.ApUser;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 /**
@@ -8,8 +9,9 @@ import org.apache.ibatis.annotations.Select;
  * @date 2022/1/23
  * @TIME:16:28
  */
+@Mapper
 public interface ApUserMapper {
 
-    @Select("select * from ap_user where phone = #{phone}")
+
     ApUser getOneByPhone(String phone);
 }
--- heima-leadnews-service/heima-leadnews-user/src/main/java/com/heima/user/service/impl/ApUserServiceImpl.java
+++ heima-leadnews-service/heima-leadnews-user/src/main/java/com/heima/user/service/impl/ApUserServiceImpl.java
@@ -9,8 +9,15 @@ import com.heima.model.user.pojos.ApUser;
 import com.heima.user.mapper.ApUserMapper;
 import com.heima.user.service.ApUserService;
 
+import com.heima.utils.common.AppJwtUtil;
+import com.heima.utils.common.MD5Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author FF
@@ -18,36 +25,52 @@ import org.springframework.stereotype.Service;
  * @TIME:17:12
  */
 @Service
-public class ApUserServiceImpl  implements ApUserService {
+public class ApUserServiceImpl implements ApUserService {
 
     @Autowired
     private ApUserMapper apUserMapper;
 
     /**
      * 用户登录
+     *
      * @param dto
      * @return
      */
     @Override
     public ResponseResult login(LoginDto dto) {
         // 非空判断
-        if(StringUtils.isEmpty(dto.getPhone()) || StringUtils.isEmpty(dto.getPassword())  ){
-            throw new CustomException(AppHttpCodeEnum.PARAM_REQUIRE);
+        if (StringUtils.isEmpty(dto.getPhone()) && StringUtils.isEmpty(dto.getPassword())) {
+            //游客  同样返回token  id = 0
+            Map<String, Object> map = new HashMap<>();
+            map.put("token", AppJwtUtil.getToken(0l));
+            return ResponseResult.okResult(map);
         }
+        // 校验码 手机号 正确性
+        Matcher matcher = Pattern.compile("^(13[0-9]|14[5|7]|15[0|1|2|3|4|5|6|7|8|9]|18[0|1|2|3|5|6|7|8|9])\\d{8}$")
+                .matcher(dto.getPhone());
+        if(!matcher.matches()){
+            return ResponseResult.errorResult(AppHttpCodeEnum.PARAM_INVALID);
+        }
+
         // 用户登录
         // 通过 手机号 搜索出用户信息
-       ApUser apUser =  apUserMapper.getOneByPhone(dto.getPhone());
-       // 非空判断
-        if(apUser == null){
-            // 用户没有用户信息  自动注册
+        ApUser apUser = apUserMapper.getOneByPhone(dto.getPhone());
+        // 非空判断
+        if (apUser == null) {
+            //抛出 没有用户异常
+            return ResponseResult.errorResult(AppHttpCodeEnum.DATA_NOT_EXIST, "用户不存在");
         }
         //  判断密码
-
-
-
-
-        // 游客登录
-
-        return null;
+        String md5 = MD5Utils.encodeWithSalt(apUser.getPassword(), apUser.getSalt());
+        if (!dto.getPassword().equals(md5)) {
+            return ResponseResult.errorResult(AppHttpCodeEnum.LOGIN_PASSWORD_ERROR);
+        }
+        // 传递 JWT
+        Map map = new HashMap<>();
+        map.put("token", AppJwtUtil.getToken(apUser.getId().longValue()));
+        apUser.setSalt("");
+        apUser.setPassword("");
+        map.put("user",apUser);
+        return ResponseResult.okResult(map);
     }
 }
